bdf7f1e1dc4f00ca6cd8400e1f63d9f407c34cba
chris <104324755+cjurgens17@users.noreply.github.com>
2023-01-17 12:06:26 -0500

    added some convenitent methods

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -120,7 +120,15 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
+
+    public Recipe addIngredients(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
